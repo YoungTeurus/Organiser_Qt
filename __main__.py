a9709aaa8f55a6152bbcea389a0789628992212d
Сергей Азаренко <s_azarenko@outlook.com>
2020-02-29 10:36:02 +0300

    test

--- __main__.py
+++ __main__.py
@@ -1,56 +1,89 @@
+'''
 import sys
+from PyQt5.QtWidgets import QApplication, QWidget
 
-import PyQt5
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
 
-import ui1 as ui
+    sys.exit(app.exec_())
+'''
 
-import Add_Task_Dialog # было
-from Add_Task_Dialog import *# я хотел
-from Task_List import *
+'''
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
 
+Form, Window = uic.loadUiType("untitled.ui")
 
-class MyApp(QMainWindow, ui.Ui_MainWindow):
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
+app.exec_()
+'''
+
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+import table, dates, organaiser_test
+
+'''class ExampleApp(QtWidgets.QMainWindow, table.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.pushButton_2.clicked.connect(self.do_stuff)  # Присоединяю к кнопке функцию
+
+    def do_stuff(self):
+        """
+        Вызывается при нажатии на левую кнопку
+        """
+        table_model = QtGui.QStandardItemModel()
+        for j in range(1,4):
+            row_of_items = list()
+            for i in range(5):
+                a = QtGui.QStandardItem(str(i*j))
+                a.setCheckable(True)
+                row_of_items.append(a)
+            table_model.appendRow(row_of_items)
+        table_model.setHorizontalHeaderLabels(["Первый", "Второй", "Третий",
+                                               "Четвёртый", "Пятый"])
+        table_model.setVerticalHeaderLabels(["A", "B", "C"])
+        self.tableView.setModel(table_model)'''
+
+'''class ExampleApp2(QtWidgets.QMainWindow, dates.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.pushButton.clicked.connect(self.do_stuff1)
+        self.pushButton_2.clicked.connect(self.do_stuff2)
+
+    def do_stuff1(self):
+        a = self.dateEdit.dateTime().toString("dd MMMM yyyy")
+        self.lineEdit.setText(a)
+
+    def do_stuff2(self):
+        a = QtCore.QDate.fromString(self.lineEdit.text(), "ddMMyyyy")
+        self.dateEdit.setDate(a)'''
+
+class ExampleOrganaiser(QtWidgets.QMainWindow, organaiser_test.Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.but_add.clicked.connect(self.add_task)
-
-    def add_task(self):
-        add_task_message_box = Add_Task_Dialog()
-        add_task_message_box.setModal(True)
-        if(add_task_message_box.exec() == QDialog.Accepted):
-            task_finded = False
-            for i in range(0, self.listWidget.count()):
-                item = self.listWidget.itemWidget(self.listWidget.item(i)) # получаем виджет из возвращенного QlistWidgetItem
-
-                if item.get_data() == add_task_message_box.task_data.text():
-                    e = item
-                    task_finded = True
-                    break
-            if task_finded and self.listWidget.count() != 0:
-                e.add_elem(QIcon("icon.png"), add_task_message_box.task_text.text(), False)
-            else:
-                e = Task_Elem_Box()
-                print(self.listWidget.sizeHint().width())
-                #e.setMaximumSize(self.listWidget.sizeHint().width(), 100)
-                #e.setMinimumSize(self.listWidget.sizeHint().width(), 100)
-                e.add_elem(QIcon("icon.png"), add_task_message_box.task_text.text(), False)
-                e.set_data(add_task_message_box.task_data.text())
-                item = QListWidgetItem(self.listWidget)
-                item.setSizeHint(e.sizeHint())
-                self.listWidget.addItem(item)
-                self.listWidget.setItemWidget(item, e)
-
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
 
 def main():
-    app = QApplication(sys.argv)  # Новый экземпляр QApplication
-    window = MyApp()  # Создаём объект класса ExampleApp
+
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleOrganaiser()  # Создаём объект класса ExampleApp
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
 
-
-if __name__ == '__main__':
-    main();
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
