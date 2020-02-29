6dc9776d2a3f39648dfaa66ea265a9bd8df01d08
Сергей Азаренко <s_azarenko@outlook.com>
2020-02-29 10:36:02 +0300

    test

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+'''
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
@@ -10,3 +11,79 @@ if __name__ == '__main__':
     w.show()
 
     sys.exit(app.exec_())
+'''
+
+'''
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+
+Form, Window = uic.loadUiType("untitled.ui")
+
+app = QApplication([])
+window = Window()
+form = Form()
+form.setupUi(window)
+window.show()
+app.exec_()
+'''
+
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtWidgets, uic, QtGui, QtCore
+import table, dates, organaiser_test
+
+'''class ExampleApp(QtWidgets.QMainWindow, table.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.pushButton_2.clicked.connect(self.do_stuff)  # Присоединяю к кнопке функцию
+
+    def do_stuff(self):
+        """
+        Вызывается при нажатии на левую кнопку
+        """
+        table_model = QtGui.QStandardItemModel()
+        for j in range(1,4):
+            row_of_items = list()
+            for i in range(5):
+                a = QtGui.QStandardItem(str(i*j))
+                a.setCheckable(True)
+                row_of_items.append(a)
+            table_model.appendRow(row_of_items)
+        table_model.setHorizontalHeaderLabels(["Первый", "Второй", "Третий",
+                                               "Четвёртый", "Пятый"])
+        table_model.setVerticalHeaderLabels(["A", "B", "C"])
+        self.tableView.setModel(table_model)'''
+
+'''class ExampleApp2(QtWidgets.QMainWindow, dates.Ui_MainWindow):
+    def __init__(self):
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.pushButton.clicked.connect(self.do_stuff1)
+        self.pushButton_2.clicked.connect(self.do_stuff2)
+
+    def do_stuff1(self):
+        a = self.dateEdit.dateTime().toString("dd MMMM yyyy")
+        self.lineEdit.setText(a)
+
+    def do_stuff2(self):
+        a = QtCore.QDate.fromString(self.lineEdit.text(), "ddMMyyyy")
+        self.dateEdit.setDate(a)'''
+
+class ExampleOrganaiser(QtWidgets.QMainWindow, organaiser_test.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+
+def main():
+
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = ExampleOrganaiser()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
---
+++ design.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\s_aza\PycharmProjects\Organiser_Qt\untitled.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(341, 100)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(20, 10, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.line = QtWidgets.QFrame(self.centralwidget)
+        self.line.setGeometry(QtCore.QRect(100, 0, 3, 61))
+        self.line.setFrameShape(QtWidgets.QFrame.VLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(110, 10, 113, 20))
+        self.lineEdit.setObjectName("lineEdit")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(10, 40, 81, 16))
+        self.label.setObjectName("label")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 341, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Тест1"))
+        self.lineEdit.setText(_translate("MainWindow", "Текст1"))
+        self.label.setText(_translate("MainWindow", "Крутой лейбл"))
